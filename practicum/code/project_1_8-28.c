d9cc125d33e9718b0d1b18dfe4afa67244ab2de8
Dale Wesa <wesa.dale@gmail.com>
2014-08-28 19:12:27 +0000

    pushing notes from 2/27

--- practicum/code/project_1_8-28.c
+++ practicum/code/project_1_8-28.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int main() {
+// variable declaration
+int firstInt;
+int secondInt;
+
+int max(int first, int second) {
 	
-	// variable declaration
-	int firstInt = 0;
-	int secondInt = 0;
+	// simply compare the two, returning the larger
+	int larger = first;
+	if ( second > larger )
+		larger = second;
+
+	return larger;
+}
+
+
+void swap(int * first, int * second) {
+	int swap = *second;
+	*second  = *first;
+	*first   = swap;	
+}
+
+
+int main() { 
+	int larger = 0;
 
 	// interface with user
 	printf("NAME: Dale Wesa \n UIN: 676019303\n");	
@@ -14,7 +33,21 @@ int main() {
 	scanf("%d", &firstInt);
 	// do the same for another variable
 	scanf("%d", &secondInt);
+
+	// pass the two input ints to our max function, storing the
+	// return value	
+
 	
-	printf("\nFirst: %d\tSecond: %d\n", firstInt, secondInt);
+	printf("\npre-swap:\n");	
+	printf("First: %d\tSecond: %d\n\n", firstInt, secondInt);
+	
+	swap(&firstInt, &secondInt);	
+
+	printf("post-swap:\n");	
+	printf("First: %d\tSecond: %d\n", firstInt, secondInt);
+	
+	larger = max(firstInt, secondInt);
+	printf("\nThe lerger number is: %d\n", larger);
 
 }
+
