0bba943ab3c26aace77b4275a4a37b7785c934e7
Dale <dale@dale-Aspire-M5-481PT.(none)>
2015-01-28 15:11:28 -0600

    pushing up progress on cs 211 proj1 for editting in lab

--- 211/code/project1Spring/proj1src.c
+++ 211/code/project1Spring/proj1src.c
@@ -6,13 +6,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//             ARRAY UTILS                 \\
+int* copyArray (int size, int* originalArray) {
+	int* newArray = calloc(size, sizeof(int));
+
+	for ( int i=0; i<size; i++ ) {
+		newArray[i] = originalArray[i];
+	} 
+	return newArray;
+}
+
+void printArr (int size, int* arr){
+
+	for( int i=0; i<size; i++)
+		printf("%d\n", arr[i]);
+}
+/////////////////////////////////////////////
+
+// needed for use with qsort
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int* linSearch (int* arr, int size, int searchingFor) {
+	int found = 0;
+	int* retVals = calloc(2, sizeof(int));
+	int i = 0;
+
+	while( !found && i<size ) {
+		if(arr[i] == searchingFor)
+			found = 1;
+		
+	}
+}
+
 int main() {
 	// initialize our variables, including a pointer to the beginning of
 	// the array of input integers. 
 	// note its size will change as we input more and more numbers
-	int *arr = calloc(1, sizeof(int));
+	int* arr = calloc(1, sizeof(int));
+	int* arrCopy;
 	int val;
 	int size = 0;
+
 
 	scanf("%d", &val);
 	while( val != -999)
@@ -25,8 +61,15 @@ int main() {
 		scanf("%d", &val);
 	}
 
-	for( int i=0; i<size; i++)
-		printf("%d\n", arr[i]);
+	arrCopy = copyArray(size, arr);
 
+	qsort(arrCopy, size, sizeof(int), cmpfunc);
+
+
+	printArr(size, arr);
+	printf("\n");
+	printArr(size, arrCopy);
+
+	
         return 0;
 }
