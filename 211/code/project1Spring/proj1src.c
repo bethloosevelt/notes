efedf61ad244074c5d7e8b3d361122d15b577341
Wesa <dwesa2@m1000101.pclabs.uic.edu>
2015-01-29 11:32:51 -0600

    211 project 1 now supports searching for alist of items in an unsorted array

--- 211/code/project1Spring/proj1src.c
+++ 211/code/project1Spring/proj1src.c
@@ -28,18 +28,53 @@ int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
 
-// not finished
+// a linear search function. returns a pointer to an array whos first int is either the slot it found the searched for 
+// number, or -1 if it was never found, and the second integer is the number of comparasins made
 int* linSearch (int* arr, int size, int searchingFor) {
 	int found = 0;
 	int* retVals = calloc(2, sizeof(int));
 	int i = 0;
+	int comparasins = 0;
 
-	while( !found && i<size ) {
-		if(arr[i] == searchingFor)
-			found = 1;
+	while( i<size ) {
 		
+		if(arr[i] == searchingFor) {
+			comparasins++;
+			retVals[0] = i;
+			retVals[1] = comparasins;
+			found = 1;
+			return retVals;
+		}
+		else {
+			comparasins++;
+			i++;
+		}
 	}
+	retVals[0] = -1;
+	retVals[1] = comparasins;
+
+	return retVals;
 }
+
+int* binSearch(int* arr, 
+
+
+
+// reads in values to a dynamic array, whether it be for sorting, or a list of values to search for
+void read(int* arr, int* size) {
+ 	 int val;
+	 scanf(" %d", &val);
+         while( val != -999)
+         {       
+                 // store the value in the array 
+                 
+                 (*size)++;
+                 arr = realloc(arr, (*size) * sizeof(int));
+                 arr[(*size)-1] = val;
+                 scanf("%d", &val);
+         }
+}
+
 
 int main() {
 	// initialize our variables, including a pointer to the beginning of
@@ -49,27 +84,34 @@ int main() {
 	int* arrCopy;
 	int val;
 	int size = 0;
+	int searchValsSize = 0;
+	int* searchVals = calloc(1, sizeof(int));
 
-
-	scanf(" %d", &val);
-	while( val != -999)
-	{
-		// store the value in the array	
-		
-		size++;
-		arr = realloc(arr, size * sizeof(int));
-		arr[size-1] = val;
-		scanf("%d", &val);
-	}
+	printf("enter numbers to fill an array, entering -999 when finished \n");
+	read(arr, &size);
 
 	arrCopy = copyArray(size, arr);
 
 	qsort(arrCopy, size, sizeof(int), cmpfunc);
 
-
+	printf("\nYour array: \n");
 	printArr(size, arr);
-	printf("\n");
+	printf("\nYour array sorted: \n");
 	printArr(size, arrCopy);
+
+	printf("\nEnter a value to search for\n");
+	read(searchVals, &searchValsSize); 
+		
+	
+	for(int i =0; i<searchValsSize; i++){
+
+		int* found = linSearch(arr, size, searchVals[i]);
+
+		if(found[0] != -1)
+			printf("\nfound %d at position %d by making %d comparasins\n", searchVals[i], found[0], found[1]);
+		else
+			printf("\nDid not find %d by making %d comparasins\n", searchVals[i], found[1]);
+	}		
 
 	
         return 0;
